f5434450d54d0e8d7ef9d72f416ce58faf341092
Night <87203483+Just-Night@users.noreply.github.com>
2026-03-03 00:14:02 +0100

    fix: refactor CustomAdminSite to use UnfoldAdminSite
    Refactor CustomAdminSite to extend UnfoldAdminSite and adjust index_title formatting.

--- services/backend/service/apps/admin/site.py
+++ services/backend/service/apps/admin/site.py
@@ -1,11 +1,15 @@
 from django.conf import settings
-from django.contrib.admin import AdminSite
+from unfold.sites import UnfoldAdminSite
 
 
-class CustomAdminSite(AdminSite):
+class CustomAdminSite(UnfoldAdminSite):
     site_header = f'{settings.PROJECT_NAME} Admin'
     site_title = f'{settings.PROJECT_NAME} Portal'
-    index_title = f'Welcome to {settings.PROJECT_NAME} admin panel '
+    index_title = f'Welcome to {settings.PROJECT_NAME} admin panel'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        return urls
 
 
 admin_site = CustomAdminSite(name='admin_site')
