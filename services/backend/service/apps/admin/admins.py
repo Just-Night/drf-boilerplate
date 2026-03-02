122c6d4431dc8cd76042a0ef9484c9de1561b343
Night <87203483+Just-Night@users.noreply.github.com>
2026-03-03 00:13:04 +0100

    fix: Register UserAdmin with custom model admin

--- services/backend/service/apps/admin/admins.py
+++ services/backend/service/apps/admin/admins.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 
 from apps.admin.site import admin_site
+from apps.admin import model_admin as main_model_admin
 
 
-admin_site.register(get_user_model())
+admin_site.register(get_user_model(), main_model_admin.UserAdmin)
