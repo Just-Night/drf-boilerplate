f532572813734fd69504be54b143dee714fc9015
Night <87203483+Just-Night@users.noreply.github.com>
2026-03-03 00:13:34 +0100

    fix: add custom admin classes for API keys and users

--- services/backend/service/apps/admin/model_admin.py
+++ services/backend/service/apps/admin/model_admin.py
@@ -1,1 +1,23 @@
-from django.contrib import admin
+from rest_framework_api_key.admin import APIKeyModelAdmin
+
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
+from unfold.admin import ModelAdmin
+
+
+class ApiKeyModelAdmin(APIKeyModelAdmin, ModelAdmin):
+    compressed_fields = True
+
+
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    # Forms loaded from `unfold.forms`
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
